fa91f0f2af07fa8f277773e35687910c85bae194
charz185 <80986803+charz185@users.noreply.github.com>
2023-09-14 00:12:38 +0000

    update 1: optimized & fancification

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import random
 import time
 import os
 import string
-import array
-import math
 import sys
-import binascii
-import base64
+
 asciiList = []
 binList = []
 
-GeneralSize = 5
+GeneralSize =  8
 
 def inputToList(input):
   global asciiList
@@ -24,8 +21,8 @@ def ListCharToBinary(charIndex):
   return (format(charIndex, '0' + str(int(len(asciiList) / 2)) + 'b'))
 
 
-def writeToBytes(inout):
-  return bytearray(inout, 'utf-8')
+def writeToBytes(input):
+  return bytearray(input, 'utf-8')
 
 
 def decToBinary(num):
@@ -52,49 +49,45 @@ def seedtoData(index, size, seed):
   temp = generateData(seed, index + size)[index:index + size]
   return (temp)
 
+
 def Jot(data1, index, seed):
   global GeneralSize
-  foundEfficientData = True
-  size = GeneralSize
-  gensize = size
-  while (foundEfficientData):
-    list2 = generateData(seed, gensize)
-    data2 = data1[index:index + size]
-    pos = list2.find(data2)
-    if pos != -1:
-      foundEfficientData = False
-      return pos
+  gensize = GeneralSize
+  data2 = data1[index:index + GeneralSize]
+  while 1:
+    int1 = generateData(seed, gensize).find(data2)
+    if int1 != -1:
+      #foundEfficientData = False
+      return int1 
     else:
-      gensize += gensize
+      gensize = int(gensize * 3)
 
+#
 
 def jot2(data1, index):
   global GeneralSize
   length = len(data1)
   seed = 0
-  while True:
+  while True :
     index = 0
     returnlist = []
-    randomSeedNotFound = True
-    while randomSeedNotFound:
+    while True:
       tic2 = time.perf_counter()
-      result = Jot(data1, index, seed)
-      toc2 = time.perf_counter()
+      returnlist.append(Jot(data1, index, seed))
       index += GeneralSize
-      returnlist.append(result)
-      
+      toc2 = time.perf_counter()
       print(str(index) + '/' + str(length) + ': ' + str((toc2 - tic2)))
       if index >= length:
-        randomSeedNotFound = False
+        break
     str1 = ""
     for x in asciiList:
       str1 += x + " "
     str1 = str1[:-1]
     str1 += ";"
-    returnlist.insert(0,seed)
+    returnlist.insert(0, seed)
     print(str(returnlist))
     for x in returnlist:
-      str1 += v2r(x, base64List)+" "
+      str1 += v2r(x, base64List) + " "
     str1 = str1[:-1]
     print(str(sys.getsizeof(str1) - sys.getsizeof(data1)) + " " + str(seed))
     if sys.getsizeof(data1) > sys.getsizeof(str1):
@@ -102,41 +95,40 @@ def jot2(data1, index):
     else:
       seed += 1
 
-def v2r(n, base): # value to representation https://stackoverflow.com/questions/54152762/python-compactly-and-reversibly-encode-large-integer-as-base64-or-base16-having
-    """
+
+def v2r(n, base):  # value to representation https://stackoverflow.com/questions/54152762/python-compactly-and-reversibly-encode-large-integer-as-base64-or-base16-having
+  """
     Convert a positive integer to its string representation in a custom base.
     
     :param n: the numeric value to be represented by the custom base
     :param base: the custom base defined as a string of characters, used as symbols of the base
     :returns: the string representation of natural number n in the custom base
     """
-    if n == 0: 
-      return base[0]
-    b = len(base)
-    digits = ''
-    while n > 0:
-        digits = base[n % b] + digits
-        n  = n // b
-    return digits
-  
-base64List='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_!@%^&+=/|~:;`[]{}()'
+  if n == 0:
+    return base[0]
+  b = len(base)
+  digits = ''
+  while n > 0:
+    digits = base[n % b] + digits
+    n = n // b
+  return digits
+
+
+base64List = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+-./:;<=>?@[\]^_`{|}~'
+
 
 #https://cs.stackexchange.com/questions/10318/the-math-behind-converting-from-any-base-to-any-base-without-going-through-base/65744#65744
-def r2v(digits, base): # representation to value
-    """Compute the number represented by string 'digits' in a custom base."""
-    b = len(base)
-    n = 0
-    for d in digits:
-        n = b * n + base[:b].index(d)
-    return n
-  
+def r2v(digits, base):  # representation to value
+  """Compute the number represented by string 'digits' in a custom base."""
+  b = len(base)
+  n = 0
+  for d in digits:
+    n = b * n + base[:b].index(d)
+  return n
+
+
 def dataToSeed(data):
-  randomSeedNotFound = True
-  counter = 0
-  timerList = []
   index = 0
-  length = len(data)
-  tic = time.perf_counter()
   returnlist = jot2(data, index)
   x = 0
   for i in os.listdir('encoded/'):
@@ -149,7 +141,7 @@ def dataToSeed(data):
 def readFile(fileName):
   with open(fileName, 'r') as f:
     a = f.readline()
-    b = a.split(';')
+    b = a.split(';',1)
     c = b[1].split(",")
     print(str(c))
     z = []
@@ -158,13 +150,13 @@ def readFile(fileName):
       z.append(x)
     inputToList(z)
     c = c[0].split(" ")
-    seed = r2v(c[0],base64List)
+    seed = r2v(c[0], base64List)
     c.pop(0)
     f.close()
     d = []
     print(str(c))
     for x in c:
-      d.append(r2v(x,base64List))
+      d.append(r2v(x, base64List))
     print(str(d))
     return [d, seed]
 
@@ -191,12 +183,13 @@ def FileToUsableDataToSeed(list1):
 random.seed(0)
 x = input("y/n")
 if x == 'y':
-  list1 = readFile('encoded/Data95.txt')
+  input = input("Encoded File # ? ")
+  list1 = readFile('encoded/Data' + input + '.txt')
   p = list1[0]
   k = ''
   list = ''
   print(str(asciiList))
-  print("P: "+str(p))
+  print("P: " + str(p))
   for x in p:
     tic1 = time.perf_counter()
     list += str(seedtoData(int(x), GeneralSize, list1[1]))
@@ -206,23 +199,23 @@ if x == 'y':
   x = 0
   for i in os.listdir('results/'):
     x += 1
-  with open("results/sample"+str(x)+".txt", "w") as f1:
+  with open("results/sample" + str(x) + ".txt", "w") as f1:
     f1.write(list)
 elif x == "Rand#":
   num = int(input())
-  str1 = ''.join(map(str,random.choices([1,2,3,4,5,6,7,8,9,0],k=num)))
-  #for z in str0:
-  #  str1 += CharToBinary(z)
+  sec = os.urandom(num)
+  sec = int.from_bytes(sec, sys.byteorder)
+  len(str(sec))
 
+  str1 = str(sec)
   inputToList(str1)
   x = 0
   for i in os.listdir('collection/'):
     x += 1
-  with open("collection/sample"+str(x)+".txt", "w") as f1:
+  with open("collection/sample" + str(x) + ".txt", "w") as f1:
     f1.write(str1)
   print(str(sys.getsizeof(str1)))
-  #z = random.choices(['0', '1'], k=m)
-  #str1 = ''.join(map(str, z))
+
   tic = time.perf_counter()
   dataToSeed(str1)
   toc = time.perf_counter()
@@ -231,38 +224,35 @@ elif x == "Image":
   y = input("Image#? ")
   imgb = ''
   GeneralSize = 4
-  with open("input/Image"+str(y)+".png", "rb") as f:
-     imgb = f.read()
-  with open("collection/imageBin.txt","wb") as f2:
+  with open("input/Image" + str(y) + ".png", "rb") as f:
+    imgb = f.read()
+  with open("collection/imageBin.txt", "wb") as f2:
     f2.write(imgb)
   print(str(sys.getsizeof(imgb)))
-  list1 = ''.join(map(str,imgb))
-  
+  #list1 = ''.join(map(str,imgb))
+  list1 = imgb
   inputToList(list1)
   print(str(asciiList))
   dataToSeed(list1)
 else:
-  #m = int(input("size")
   file = input("file? ")
   str0 = ''
-  with open("input/Data"+file+".txt") as f:
-    str0 = f.readlines()[0]
-    
-  #with open("test.txt",'r') as f:
-   # x = f.readlines()
-  str1 = str0
-  #for z in str0:
-  #  str1 += CharToBinary(z)
-
+  with open("input/Data" + file + ".txt") as f:
+    str0 = f.readlines()
+  str1 = ''
+  if len(str0) > 1:
+    for x in str0:
+      str1 += x
+  else:
+    str1 = str0[0]
+  
   inputToList(str1)
   x = 0
   for i in os.listdir('collection/'):
     x += 1
-  with open("collection/sample"+str(x)+".txt", "w") as f1:
+  with open("collection/sample" + str(x) + ".txt", "w") as f1:
     f1.write(str1)
   print(str(sys.getsizeof(str1)))
-  #z = random.choices(['0', '1'], k=m)
-  #str1 = ''.join(map(str, z))
   tic = time.perf_counter()
   dataToSeed(str1)
   toc = time.perf_counter()
---
+++ readme.md
@@ -0,0 +1,9 @@
+This program is for compressing text by seed-based random generation.
+The main use for this program is for compressing "non-compressible" strings.
+Tips to making this program work:
+1. Use the least amount of unique characters as possible. Don't type in a a piece of text with more than about 10 types of characters.
+2. Use text that is long (length>600 characters), dont try to compress your, unless you have the longest name in the world.
+
+Rules for using this program:
+- businesses/companies are not allowed to use this program unless they have permission from the creator of this program.
+- You are not allowed to train AI off of this program! Nor can you use ai to contribute.
